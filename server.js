c2d4af18a77da2b12f72c8c0fb243fe26c64cb01
ChloeKonde <konde.chloe@gmail.com>
2021-09-13 14:59:49 +0600

    add express serves

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/angular-app'));
+
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + 'dist/angular-app/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8080);
